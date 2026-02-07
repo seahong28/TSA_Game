22bc4c504e43c933f0dd14cecd66733fd0da5b54
alleykim0722 <alleykim0722@gmail.com>
2026-02-07 18:12:10 -0500

    fixed lift to stop at top
    lift would not stop at top and player could not beat level

--- alice - tsa game/Assets/script/Level2/elevator.gd
+++ alice - tsa game/Assets/script/Level2/elevator.gd
@@ -1,32 +1,20 @@
 extends PathFollow2D
 
 @export var speed := 0.05
-var move:= false
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var stop_y := -935.0   
 
+var move := false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if move:
 		progress_ratio += speed * delta
-	
-			
-
-	# update child position manuall 
-	
+		
+		# stop when we reach the target height
+		if global_position.y <= stop_y:
+			move = false
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print("entered")
-		move =  true
-		
-
-
-func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		print("left")
-		move =  false
-		progress_ratio = 0.0
+		move = true
