f47c88ef3bcae599efc45a5975b510b6d04d2d33
seahong28 <seahong28@gmail.com>
2026-02-10 21:36:01 -0500

    lift fixed

--- alice - tsa game/Assets/script/Level2/elevator.gd
+++ alice - tsa game/Assets/script/Level2/elevator.gd
@@ -18,3 +18,8 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print("entered")
 		move = true
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		print("left")
+		move =  false
+		progress_ratio = 0.0
